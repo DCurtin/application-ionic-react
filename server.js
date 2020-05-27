82aa00014077aa84fb55f8391ab79f7344246450
Dalton Curtin <daltoncurtin@gmail.com>
2020-05-27 19:47:34 -0400

    Adding server commands

--- server.js
+++ server.js
@@ -9,6 +9,25 @@ var {
 } = require('pg');
 var jsforce = require('jsforce');
 
+var serverConn = new jsforce.Connection({
+  oauth2 : {
+    // you can change loginUrl to connect to sandbox or prerelease env.
+    loginUrl : 'https://test.salesforce.com',
+    //loginUrl : 'https://dcurtin-iraonline.cs17.force.com/client',
+    clientId : process.env.SFServer_Id,
+    clientSecret : process.env.SFServer_Sec,
+    redirectUri : process.env.SF_Redirect
+  }
+});
+
+serverConn.login(process.env.UserId, process.env.UserPw + process.env.UserToken, function(err, userInfo) {
+  console.log('token: ' + serverConn.accessToken)
+  if (err) {
+    console.log(err);
+    return console.log('fail');
+  }
+})
+
 
 var app = express();
 app.use(session({
@@ -38,6 +57,95 @@ app.use(router);
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
+
+app.post('/startApplication', function(req, res){
+  var session = req.body.session;
+  //establish application, generate session
+  //return sessionId
+
+  var onlineAppData = req.body.data;
+  var session = req.body.session;
+  var page = session.page;
+  var sessionId = session.sessionId;
+
+  if(sessionId !== undefined){
+    console.log('application must be started first, a step was skipped or the session was lost');
+    console.log(sessionId);
+    res.status(500).send('SessionId not set');
+    return;
+  }
+  //figure out what page they're on
+  const hash = require('crypto').createHash('sha256');
+  var token = hash.update(JSON.stringify(onlineAppData) + Math.random.toString()).digest('hex');
+
+  onlineAppData['Dedicated_Rep__c'] = '0050M00000Dv1h5QAB';
+  onlineAppData['Token__c'] = token;
+  const insertAppQuery = {
+    text: 'INSERT INTO salesforce.application__c(first_name__c, last_name__c, email__c, token__c) VALUES($1, $2, $3, $4)',
+    values: [onlineAppData['First_Name__c'], onlineAppData['Last_Name__c'], onlineAppData['Email__c'], token],
+  }
+  client.query(insertAppQuery, function(err, response){
+    console.log("response");
+    console.log(response);
+    console.log("err");
+    console.log(err);
+    res.json({'sessionId': token});
+  });
+});
+
+app.post('/saveState', function(req, res){
+  console.log(serverConn);
+  console.log(req.body);
+  var onlineAppData = req.body.data;
+  var session = req.body.session;
+  var page = session.page;
+  var sessionId = session.sessionId;
+
+  if(sessionId === undefined){
+    console.log('application must be started first, a step was skipped or the session was lost');
+    console.log(sessionId);
+    res.status(500).send('SessionId not set');
+    return;
+  }
+  //figure out what page the user is on, upsert data
+
+});
+
+app.post('/saveApplication', function(req, res){
+  var session = req.body.session;
+
+  //merge data in forms
+  //insert or upsert data to salesforce
+  
+  res.send('ok');
+})
+
+app.post('/getPageFields', function(req, res){
+  var session = req.body.session;
+
+  //get data from database
+  //load into response
+  res.send('ok');
+})
+
+function insertApplication(onlineApp){
+  serverConn.sobject("Online_Application__c").create(onlineApp, function(err, ret){
+    console.log(ret);
+    if(ret.success === true){
+      
+    }
+  });
+}
+
+function updateApplication(){
+  serverConn.sobject("Online_Application__c").create(onlineApp, function(err, ret){
+    console.log(ret);
+    if(ret.success === true){
+      
+    }
+  });
+}
+
 
 
 
