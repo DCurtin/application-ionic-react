df4aa627855c9eac7af2e2d4e19240062bb5da4f
Dalton Curtin <daltoncurtin@gmail.com>
2020-06-09 09:10:44 -0400

    Adding some potential beneficiary interfaces

--- client/src/helpers/Utils.tsx
+++ client/src/helpers/Utils.tsx
@@ -65,4 +65,25 @@ export interface FormData {
     [key:string] : any
 }
 
+export interface beneficiaryForm{
+    beneficiary_count__c: number
+    beneficiaries: Array<beneficiary>
+}
+
+export interface beneficiary{
+beneficiary_first_name: string,
+beneficiary_last_name: string,
+beneficiary_ssn: string,
+beneficiary_dob: string,
+beneficiary_type: string,
+beneficiary_relationship: string,
+beneficiary_share: string,
+beneficiary_street: string,
+beneficiary_city: string, 
+beneficiary_state: string,
+beneficiary_zip: string,
+beneficiary_phone: string,
+beneficiary_email: string
+}
+
 export const states = [ 'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FL', 'GA',  'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY' ];
