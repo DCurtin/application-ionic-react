6ad86ce3fb7c0b5543ff5d0decb0d6916e1d2a1f
Dalton Curtin <daltoncurtin@gmail.com>
2020-06-09 09:08:11 -0400

    Adding interface currently being used by beneficiaries

--- client/src/helpers/Utils.tsx
+++ client/src/helpers/Utils.tsx
@@ -61,4 +61,8 @@ export interface applicantId{
     alternatePhoneType: string
 }
 
+export interface FormData {
+    [key:string] : any
+}
+
 export const states = [ 'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FL', 'GA',  'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY' ];
