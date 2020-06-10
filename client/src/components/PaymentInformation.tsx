b1c744ce8297a707a9530b8b6b9cd4790a3753c5
Will Johnston <wjohnston@midlandira.com>
2020-06-10 15:44:06 -0500

    Added setup fee header and submit button

--- client/src/components/PaymentInformation.tsx
+++ client/src/components/PaymentInformation.tsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import {SessionApp} from '../helpers/Utils';
-import { IonContent, IonGrid, IonRow, IonCol } from '@ionic/react';
+import { IonContent, IonGrid, IonRow, IonCol, IonItemDivider, IonText, IonButton } from '@ionic/react';
 
 const PaymentInformation: React.FC<SessionApp> = ({sessionId, setSessionId}) => {
     return (
@@ -12,6 +12,16 @@ const PaymentInformation: React.FC<SessionApp> = ({sessionId, setSessionId}) => 
                         Midland charges your credit card for today's non-refundable fee(s) listed below. Future annual fees will be charged to the same card unless notified otherwise.
                         </p>
                     </IonCol>
+                </IonRow>
+                <IonItemDivider>
+                    <strong>
+                        <IonText color='primary'>
+                            $50 Setup Fee
+                        </IonText>
+                    </strong>
+                </IonItemDivider>
+                <IonRow>
+                    <IonButton color="primary">Submit & Proceed</IonButton>
                 </IonRow>
             </IonGrid>
         </IonContent>
