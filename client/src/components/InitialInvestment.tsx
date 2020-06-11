10874cb7bc4865a7728936999c7ede88345a9b18
Michelle Yuen <myuen@midlandira.com>
2020-06-11 14:16:26 -0500

    Update InitialInvestment.tsx

--- client/src/components/InitialInvestment.tsx
+++ client/src/components/InitialInvestment.tsx
@@ -1,12 +1,15 @@
 import React, {useState, useEffect} from 'react';
 import { SessionApp, initialInvestmentTypes } from '../helpers/Utils';
-import { IonContent, IonGrid, IonRow, IonCol, IonItemDivider, IonText, IonLabel, IonSelect, IonSelectOption } from '@ionic/react';
+import { IonContent, IonGrid, IonRow, IonCol, IonItemDivider, IonText, IonLabel, IonSelect, IonSelectOption, IonInput } from '@ionic/react';
 
 const InitialInvestment : React.FC<SessionApp> = ({sessionId, setSessionId}) => {   
-    const [formData, setFormData] = useState({
-        initial_investment_type__c : 'Futures/Forex'
-    });
+    let investmentTypesArr = initialInvestmentTypes.filter(investmentType => (investmentType !== `I'm Not Sure`));
 
+    const [formData, setFormData] = useState({
+        initial_investment_type__c : 'Futures/Forex',
+        initial_investment_name__c: ''
+    });
+    
     useEffect(() => {
         // TO DO: Grab initial investment type fields from saved application 
     })
@@ -17,7 +20,20 @@ const InitialInvestment : React.FC<SessionApp> = ({sessionId, setSessionId}) => 
         setFormData(prevState => ({...prevState, [e.target.name]:newValue}));
     }
 
-    let investmentTypesArr = initialInvestmentTypes.filter(investmentType => (investmentType !== `I'm Not Sure`));
+    const displayEntityNameLabel = (investmentType:string) => {
+        let entityNameLabel = 'Entity Name';
+        if (investmentType.includes('Note')){
+            entityNameLabel = 'Borrower Name';
+        } else if (investmentType === 'Real Estate'){
+            entityNameLabel = 'Property Address';
+        } else if (investmentType === 'Futures/Forex'){
+            entityNameLabel = 'Trading Company Name';
+        } else if (investmentType === 'Precious Metals') {
+            entityNameLabel = 'Dealer Name';
+        }
+        return entityNameLabel;
+    }
+    
 
     return (
         <IonContent className='ion-padding'>
@@ -44,6 +60,10 @@ const InitialInvestment : React.FC<SessionApp> = ({sessionId, setSessionId}) => 
                                 <IonSelectOption key={index} value={investmentType}>{investmentType}</IonSelectOption>
                             ))}
                         </IonSelect>
+                    </IonCol>
+                    <IonCol>
+                            <IonLabel>{displayEntityNameLabel(formData.initial_investment_type__c)}</IonLabel>
+                            <IonInput name='initial_investment_name__c' value={formData.initial_investment_name__c}></IonInput>
                     </IonCol>
                 </IonRow>
             </IonGrid>
