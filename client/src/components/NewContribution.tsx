24d69b7c2ceac95a5a7e40a3a02962ea6ff15f71
Michelle Yuen <myuen@midlandira.com>
2020-06-11 15:49:35 -0500

    Create NewContribution.tsx

---
+++ client/src/components/NewContribution.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { SessionApp } from '../helpers/Utils';
+import { IonContent } from '@ionic/react';
+
+const NewContribution: React.FC<SessionApp> = ({sessionId, setSessionId}) => {
+
+    return (
+        <IonContent className='ion-padding'>
+
+        </IonContent>
+    );
+}
+export default NewContribution;
