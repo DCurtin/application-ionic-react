18468b3499226f96ef9ef32766aee76023f104fc
Dalton Curtin <daltoncurtin@gmail.com>
2020-06-15 13:53:49 -0400

    Updating get page info

--- server/utils/getPageInfoHandlers.ts
+++ server/utils/getPageInfoHandlers.ts
@@ -1,4 +1,4 @@
-import {saveWelcomeParameters, requestBody, welcomePageParameters, saveApplicationId, applicantIdForm, beneficiary, beneficiaryPlaceHolder, feeArrangementForm, accountNotificationsForm, transferForm} from '../../client/src/helpers/Utils'
+import {saveWelcomeParameters, requestBody, welcomePageParameters, saveApplicationId, applicantIdForm, beneficiary, feeArrangementForm, accountNotificationsForm, transferForm} from '../../client/src/helpers/Utils'
 import * as salesforceSchema from './salesforce'
 import {addressSchema, identificationSchema} from './helperSchemas'
 import {transformBeneficiariesServerToClient} from '../utils/transformBeneficiaries'
@@ -43,28 +43,8 @@ export function handleApplicationIdPage(sessionId: string, res: express.Response
           res.json({data:applicantInfo});
           return;
         }
-        let identification : identificationSchema = <identificationSchema>applicantInfo?.identification;
-        let legalAddress : addressSchema = <addressSchema>applicantInfo?.legal_address;
-        let mailingAddress: addressSchema = <addressSchema>applicantInfo?.mailing_address;
-  
-        let data : applicantIdForm = applicantInfo as applicantIdForm
-        data.confirm_email = data.email;
-        
-        data.mailing_street = mailingAddress.address
-        data.mailing_city = mailingAddress.city
-        data.mailing_state = mailingAddress.state
-        data.mailing_zip = mailingAddress.zip
-
-        data.legal_street = legalAddress.address
-        data.legal_city = legalAddress.city
-        data.legal_state = legalAddress.state
-        data.legal_zip = legalAddress.zip
-
-        data.id_number = identification.idNumber
-        data.id_type = identification.idType
-        data.issue_date = identification.issueDate
-        data.expiration_date = identification.expirationDate
-
+ 
+        let data : applicantIdForm = applicantInfo as applicantIdForm        
         res.json({'data': data})
       }).catch(err=>{
         res.status(500).send('failed getting apllicant data');
@@ -79,7 +59,7 @@ export function handleBeneficiaryPage(sessionId: string, res: express.Response, 
 
   client.query(beneQuery).then( function( result : pg.QueryResult){
   let beneficiaryList : Array<salesforceSchema.beneficiary> = result.rows;
-  let returnData : beneficiaryPlaceHolder = transformBeneficiariesServerToClient(beneficiaryList)
+  let returnData = transformBeneficiariesServerToClient(beneficiaryList)
   res.json({data:returnData});
   }).catch(err=>{
     res.status(500).send('failed getting bene data');
@@ -102,18 +82,13 @@ export function handleFeeArrangementPage(sessionId:string, res: express.Response
     if(feeArrangementData === undefined)
     {
       res.send('no rows');
+      return
     }
 
     client.query(bodyQuery).then( function(bodyResult:pg.QueryResult){
     let investMentType : string = bodyResult.rows[0]?.investment_type
 
-    let feeArrangementForm : feeArrangementForm ={
-      cc_exp_date__c: feeArrangementData.expiration_date,
-      fee_schedule__c: feeArrangementData.fee_agreement,
-      cc_number__c: feeArrangementData.credit_number,
-      payment_method__c: feeArrangementData.payment_method,
-      initial_investment_type__c: investMentType
-    }
+    let feeArrangementForm : feeArrangementForm = {...feeArrangementData, initial_investment_type: investMentType};
     res.json({data:feeArrangementForm});
     })
   }).catch(err=>{
@@ -129,24 +104,7 @@ export function handleAccountNotificationPage(sessionId:string, res: express.Res
 
   client.query(interestedPartyQuery).then(function(result : pg.QueryResult){
     let interestedPartyInfo : salesforceSchema.interested_party = result.rows[0];
-    let ipAddress : addressSchema = interestedPartyInfo.address as addressSchema
-    let accountNotificationsForm : accountNotificationsForm = {
-      interested_party_access_level__c: interestedPartyInfo.access_level,
-      statement_option__c: interestedPartyInfo.statement_option,
-      interested_party_street__c: ipAddress.address,
-      interested_party_city__c: ipAddress.city,
-      interested_party_state__c: ipAddress.state,
-      interested_party_zip__c: ipAddress.zip,
-      interested_party_company_name__c: interestedPartyInfo.company_name,
-      interested_party_email__c: interestedPartyInfo.email,
-      interested_party_email_notifications__c: interestedPartyInfo.email_notifications,
-      interested_party_phone__c: interestedPartyInfo.phone,
-      interested_party_first_name__c: interestedPartyInfo.first_name,
-      interested_party_last_name__c: interestedPartyInfo.last_name,
-      interested_party_ira_statement__c: interestedPartyInfo.ira_statement,
-      interested_party_online_access__c: interestedPartyInfo.online_access,
-      interested_party_title__c: interestedPartyInfo.title
-    }
+    let accountNotificationsForm : accountNotificationsForm = interestedPartyInfo;
     res.json({data:accountNotificationsForm});
   }).catch(err=>{
     res.status(500).send('failed getting interested party data');
