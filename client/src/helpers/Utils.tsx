1a53e1f3aeb531057b7956ed0d860bfc78ad2b70
Dalton Curtin <daltoncurtin@gmail.com>
2020-06-15 12:38:09 -0400

    Refactored all fields getting rid of __c and renaming some/ removing unused or unecessary fields

--- client/src/helpers/Utils.tsx
+++ client/src/helpers/Utils.tsx
@@ -35,8 +35,7 @@ export interface applicantIdForm{
     first_name?: string, 
     last_name?: string, 
     ssn?: string, 
-    email?: string, 
-    confirm_email?: string,
+    email?: string
     dob?: string, 
     salutation?: string,
     marital_status?: string,
@@ -44,9 +43,9 @@ export interface applicantIdForm{
     occupation?: string,
     id_type?: string, 
     id_number?: string,
-    issued_by?:string, 
-    issue_date?: string, 
-    expiration_date?: string,
+    id_issued_by?:string, 
+    id_issued_date?: string, 
+    id_expiration_date?: string,
     legal_street?: string, 
     legal_city?: string, 
     legal_state?:string,
@@ -71,84 +70,20 @@ export interface beneficiaryForm{
 }
 
 export interface beneficiary{
-beneficiary_first_name: string,
-beneficiary_last_name: string,
-beneficiary_ssn: string,
-beneficiary_dob: string,
-beneficiary_type: string,
-beneficiary_relationship: string,
-beneficiary_share: string,
-beneficiary_street: string,
-beneficiary_city: string, 
-beneficiary_state: string,
-beneficiary_zip: string,
-beneficiary_phone: string,
-beneficiary_email: string
-beneficiary_token: string
-}
-
-export interface saveBeneficiary extends requestBody{
-    data: beneficiaryPlaceHolder
-}
-
-export interface beneficiaryPlaceHolder{
-    beneficiary_count__c: number, 
-    beneficiary_first_name_1__c: string,
-    beneficiary_last_name_1__c:string,
-    beneficiary_ssn_1__c:string,
-    beneficiary_dob_1__c:string,
-    beneficiary_type_1__c: string,
-    beneficiary_relationship_1__c:string,
-    beneficiary_share_1__c:string,
-    beneficiary_street_1__c: string,
-    beneficiary_city_1__c:string, 
-    beneficiary_state_1__c:string,
-    beneficiary_zip_1__c: string,
-    beneficiary_phone_1__c: string,
-    beneficiary_email_1__c: string,
-    beneficiary_token_1__c: string,
-    beneficiary_first_name_2__c: string,
-    beneficiary_last_name_2__c: string, 
-    beneficiary_ssn_2__c: string,
-    beneficiary_dob_2__c: string,
-    beneficiary_type_2__c: string,
-    beneficiary_relationship_2__c:string,
-    beneficiary_share_2__c: string,
-    beneficiary_street_2__c: string,
-    beneficiary_city_2__c:string, 
-    beneficiary_state_2__c:string,
-    beneficiary_zip_2__c: string,
-    beneficiary_phone_2__c: string,
-    beneficiary_email_2__c: string,
-    beneficiary_token_2__c: string,
-    beneficiary_first_name_3__c: string,
-    beneficiary_last_name_3__c: string, 
-    beneficiary_ssn_3__c: string, 
-    beneficiary_dob_3__c: string,
-    beneficiary_type_3__c: string,
-    beneficiary_relationship_3__c:string,
-    beneficiary_share_3__c: string,
-    beneficiary_street_3__c: string,
-    beneficiary_city_3__c:string, 
-    beneficiary_state_3__c:string,
-    beneficiary_zip_3__c: string,
-    beneficiary_phone_3__c: string,
-    beneficiary_email_3__c: string,
-    beneficiary_token_3__c: string,
-    beneficiary_first_name_4__c: string,
-    beneficiary_last_name_4__c: string,
-    beneficiary_ssn_4__c:string,
-    beneficiary_dob_4__c:string,
-    beneficiary_type_4__c:string,
-    beneficiary_relationship_4__c:string,
-    beneficiary_share_4__c:string,
-    beneficiary_street_4__c: string,
-    beneficiary_city_4__c:string, 
-    beneficiary_state_4__c:string,
-    beneficiary_zip_4__c: string,
-    beneficiary_phone_4__c: string,
-    beneficiary_email_4__c: string,
-    beneficiary_token_4__c: string
+first_name: string,
+last_name: string,
+ssn: string,
+dob: string,
+type: string,
+relationship: string,
+share_percentage: number,
+mailing_street: string,
+mailing_city: string, 
+mailing_state: string,
+mailing_zip: string,
+phone: string,
+email: string
+token: string
 }
 
 export interface saveFeeArrangement extends requestBody{
@@ -156,55 +91,54 @@ export interface saveFeeArrangement extends requestBody{
 }
 
 export interface feeArrangementForm{
-    initial_investment_type__c: string,
-    fee_schedule__c: string,
-    payment_method__c: string,
-    cc_number__c: string,
-    cc_exp_date__c: string
+    initial_investment_type: string,
+    fee_schedule: string,
+    payment_method: string,
+    cc_number: string,
+    cc_exp_date: string
 }
 export const initialInvestmentTypes = ['I\'m Not Sure', 'Futures/Forex', 'Closely-Held LLC', 'Private Placement', 'Promissory Note (Unsecured)', 'Promissory Note (Secured by Real Estate)', 'Promissory Note (Secured by Other)', 'Precious Metals', 'Real Estate', 'Other'];
 
 export interface accountNotificationsForm{
-    statement_option__c: string, 
-    interested_party_email_notifications__c: boolean,
-    interested_party_access_level__c: string,
-    interested_party_first_name__c: string, 
-    interested_party_last_name__c: string, 
-    interested_party_email__c: string, 
-    interested_party_phone__c:string, 
-    interested_party_street__c: string, 
-    interested_party_city__c: string,
-    interested_party_state__c: string,
-    interested_party_zip__c: string, 
-    interested_party_company_name__c: string,
-    interested_party_title__c: string, 
-    interested_party_online_access__c: boolean,
-    interested_party_ira_statement__c: string
+    ira_statement_option: string
+    online_access: boolean,
+    include_interested_party: boolean,
+    statement_option: string, 
+    first_name: string, 
+    last_name: string, 
+    email: string, 
+    phone:string, 
+    mailing_street: string,
+    mailing_city: string,
+    mailing_state: string,
+    mailing_zip: string,
+    company_name: string,
+    title: string, 
 }
 
 export interface transferForm{
-    existing_ira_transfers: number,
+    existing_transfers: number,
     account_type: string,
     transfers: Array<transfer>
 }
 
 export interface transfer{
-    ira_account_number: string,
-    ira_institution_name: string,
-    ira_contact_name: string,
-    ira_contact_phone_number: string,
-    ira_street: string,
-    ira_city: string,
-    ira_state: string,
-    ira_zip: string,
+    account_number: string,
+    institution_name: string,
+    contact_name: string,
+    contact_phone_number: string,
+    mailing_street: string,
+    mailing_city: string,
+    mailing_state: string,
+    mailing_zip: string,
+    account_type: string,
     transfer_type: string,
-    ira_account_type: string,
     delivery_method: string,
-    transfer_assetname1: string, //number mid
-    transfer_assetname2: string, //number mid
-    transfer_assetname3: string, //number mid
-    ira_full_or_partial_cash_transfer: string,
-    ira_cash_amount: number,
+    asset_name_1: string, //number mid
+    asset_name_2: string, //number mid
+    asset_name_3: string, //number mid
+    full_or_partial_cash_transfer: string,
+    cash_amount: number,
     index: number
 }
 
