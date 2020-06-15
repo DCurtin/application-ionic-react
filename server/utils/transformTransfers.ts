bc1b8257e29b4fbc6904ad965db44d9193beef81
Dalton Curtin <daltoncurtin@gmail.com>
2020-06-15 13:56:50 -0400

    Updatings to transform functions related to field refactoring

--- server/utils/transformTransfers.ts
+++ server/utils/transformTransfers.ts
@@ -4,31 +4,31 @@ import {addressSchema} from '../utils/helperSchemas'
 
 export function transformTransferClientToServer(transferFlattened: any): transferForm{
     let transferForm : transferForm ={
-        account_type: transferFlattened['account_type__c'],
-        existing_ira_transfers: transferFlattened['existing_ira_transfers__c'],
+        account_type: transferFlattened['account_type'],
+        existing_transfers: transferFlattened['existing_transfers'],
         transfers: []
     }
-    let transferCount : number = transferForm.existing_ira_transfers;
-    console.log('existing '+transferFlattened['existing_ira_transfers__c'])
+    let transferCount : number = transferForm.existing_transfers;
+    console.log('existing '+transferFlattened['existing_transfers'])
 
     for(let index = 1; index < (transferCount + 1); ++index){
         let castedTransfer : transfer ={
-            ira_account_number: transferFlattened[`ira_account_number_${index}__c`],
-            ira_account_type: transferFlattened[`ira_account_type_${index}__c`],
-            ira_cash_amount: transferFlattened[`ira_cash_amount_${index}__c`],
-            ira_contact_name: transferFlattened[`ira_contact_name_${index}__c`],
-            ira_contact_phone_number: transferFlattened[`ira_contact_phone_number_${index}__c`],
-            ira_full_or_partial_cash_transfer: transferFlattened[`ira_full_or_partial_cash_transfer_${index}__c`],
-            ira_institution_name: transferFlattened[`ira_institution_name_${index}__c`],
-            transfer_type: transferFlattened[`transfer_type_${index}__c`],
-            delivery_method: transferFlattened[`delivery_method_${index}__c`],
-            transfer_assetname1: transferFlattened[`transfer${index}assetname1__c`],
-            transfer_assetname2: transferFlattened[`transfer${index}assetname2__c`],
-            transfer_assetname3: transferFlattened[`transfer${index}assetname3__c`],
-            ira_street: transferFlattened[`ira_street_${index}__c`],
-            ira_city: transferFlattened[`ira_city_${index}__c`],
-            ira_state: transferFlattened[`ira_state_${index}__c`],
-            ira_zip: transferFlattened[`ira_zip_${index}__c`],
+            account_number: transferFlattened[`account_number__${index}`],
+            account_type: transferFlattened[`account_type__${index}`],
+            cash_amount: transferFlattened[`cash_amount__${index}`],
+            contact_name: transferFlattened[`contact_name__${index}`],
+            contact_phone_number: transferFlattened[`contact_phone_number__${index}`],
+            full_or_partial_cash_transfer: transferFlattened[`full_or_partial_cash_transfer__${index}`],
+            institution_name: transferFlattened[`institution_name__${index}`],
+            transfer_type: transferFlattened[`transfer_type__${index}`],
+            delivery_method: transferFlattened[`delivery_method__${index}`],
+            asset_name_1: transferFlattened[`asset_name_1__${index}`],
+            asset_name_2: transferFlattened[`asset_name_2__${index}`],
+            asset_name_3: transferFlattened[`asset_name_3__${index}`],
+            mailing_street: transferFlattened[`mailing_street__${index}`],
+            mailing_city: transferFlattened[`mailing_city__${index}`],
+            mailing_state: transferFlattened[`mailing_state__${index}`],
+            mailing_zip: transferFlattened[`mailing_zip__${index}`],
             index: index
         }
         transferForm.transfers.push(castedTransfer);
@@ -40,28 +40,27 @@ export function transformTransferClientToServer(transferFlattened: any): transfe
 
 export function transformTransferServerToClient(transferForm: Array<salesforceSchema.transfer>, accountType: string):any{
     let returnData : any = {};
-    returnData['account_type__c'] = accountType; //query from applicant or body
-    returnData['existing_ira_transfers__c'] =transferForm.length;
+    returnData['account_type'] = accountType; //query from applicant or body
+    returnData['existing_transfers'] =transferForm.length;
     let index = 0;
     transferForm.forEach(transfer =>{
       ++index;
-      let transferAddress :addressSchema = transfer.address as addressSchema
-      returnData[`ira_account_number_${index}__c`] = transfer.account_number;
-      returnData[`ira_account_type_${index}__c`] = transfer.account_type;
-      returnData[`ira_cash_amount_${index}__c`] = transfer.amount;
-      returnData[`ira_contact_name_${index}__c`] = transfer.contact_name
-      returnData[`ira_contact_phone_number_${index}__c`] = transfer.contact_phone_number
-      returnData[`ira_full_or_partial_cash_transfer_${index}__c`] = transfer.full_or_partial
-      returnData[`ira_institution_name_${index}__c`] = transfer.institution_name
-      returnData[`transfer_type_${index}__c`] = transfer.transfer_type
-      returnData[`transfer${index}assetname1__c`] = transfer.asset_name_1
-      returnData[`transfer${index}assetname2__c`] = transfer.asset_name_2
-      returnData[`transfer${index}assetname3__c`] = transfer.asset_name_3
-      returnData[`ira_street_${index}__c`] = transferAddress.address
-      returnData[`ira_city_${index}__c`] = transferAddress.city
-      returnData[`ira_state_${index}__c`] = transferAddress.state
-      returnData[`ira_zip_${index}__c`] = transferAddress.zip
-      returnData[`delivery_method_${index}__c`] = transfer.delivery_method
+      returnData[`account_number__${index}`] = transfer.account_number;
+      returnData[`account_type__${index}`] = transfer.account_type;
+      returnData[`cash_amount__${index}`] = transfer.cash_amount;
+      returnData[`contact_name__${index}`] = transfer.contact_name
+      returnData[`contact_phone_number__${index}`] = transfer.contact_phone_number
+      returnData[`full_or_partial_cash_transfer__${index}`] = transfer.full_or_partial_cash_transfer
+      returnData[`institution_name__${index}`] = transfer.institution_name
+      returnData[`transfer_type__${index}`] = transfer.transfer_type
+      returnData[`asset_name_1__${index}`] = transfer.asset_name_1
+      returnData[`asset_name_2__${index}`] = transfer.asset_name_2
+      returnData[`asset_name_3__${index}`] = transfer.asset_name_3
+      returnData[`mailing_street__${index}`] = transfer.mailing_street
+      returnData[`mailing_city__${index}`] = transfer.mailing_city
+      returnData[`mailing_state__${index}`] = transfer.mailing_state
+      returnData[`mailing_zip__${index}`] = transfer.mailing_zip
+      returnData[`delivery_method__${index}`] = transfer.delivery_method
     })
   
     return returnData;
