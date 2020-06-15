5c985cadadaabfa8c01952ae1aec5a153e58a6b0
Dalton Curtin <daltoncurtin@gmail.com>
2020-06-15 13:56:25 -0400

    Updatings to save state

--- server/utils/saveStateHandlers.ts
+++ server/utils/saveStateHandlers.ts
@@ -39,24 +39,7 @@ export function saveTransferPage(sessionId: string, transferForm: transferForm, 
 function updateTransfer(token: string, transferForm: transferForm): queryParameters{
   let upsertTransferList  : Array<salesforceSchema.transfer> = []
   transferForm.transfers.forEach(element => {
-    let upsertTransfer: salesforceSchema.transfer = {
-      account_number: element.ira_account_number,
-      account_type: element.ira_account_type,
-      amount: element.ira_cash_amount,
-      contact_name: element.ira_contact_name,
-      contact_phone_number: element.ira_contact_phone_number,
-      delivery_method: element.delivery_method,
-      full_or_partial: element.ira_full_or_partial_cash_transfer,
-      institution_name: element.ira_institution_name,
-      address: generateTransferFormAddress(element),
-      asset_name_1: element.transfer_assetname1,
-      asset_name_2: element.transfer_assetname2,
-      asset_name_3: element.transfer_assetname3,
-      transfer_type: element.transfer_type,
-      index: element.index,
-      key: token + element.index,
-      token: token
-    }
+    let upsertTransfer: salesforceSchema.transfer = {...element, key: token+element.index, token: token}
     upsertTransferList.push(upsertTransfer)
   });
   return generateQueryStringFromList('transfer', upsertTransferList, 'key');
@@ -64,31 +47,17 @@ function updateTransfer(token: string, transferForm: transferForm): queryParamet
 
 function updateAccountNotifications(token: string, accountNotificationsForm: accountNotificationsForm): queryParameters
 {
-  let upsertAccountNotificationsParameters: salesforceSchema.interested_party ={
-    statement_option : accountNotificationsForm.statement_option__c,
-    access_level: accountNotificationsForm.interested_party_access_level__c,
-    address : generateIPAddress(accountNotificationsForm),
-    company_name: accountNotificationsForm.interested_party_company_name__c,
-    email : accountNotificationsForm.interested_party_email__c,
-    email_notifications : accountNotificationsForm.interested_party_email_notifications__c,
-    first_name : accountNotificationsForm.interested_party_first_name__c,
-    middle_name : '',
-    last_name : accountNotificationsForm.interested_party_last_name__c,
-    phone : accountNotificationsForm.interested_party_phone__c,
-    title : accountNotificationsForm.interested_party_title__c,
-    online_access : accountNotificationsForm.interested_party_online_access__c,
-    ira_statement : accountNotificationsForm.interested_party_ira_statement__c,
-    token : token
-  }
+  let upsertAccountNotificationsParameters: salesforceSchema.interested_party ={...accountNotificationsForm, token:token}
   return generateQueryString('interested_party', upsertAccountNotificationsParameters, 'token')
 }
 
 function updateFeeArrangementPage(token: string, feeArrangementForm: feeArrangementForm): queryParameters{
-  let upsertFeeArrangementParamters : salesforceSchema.fee_arrangement ={
-    credit_number: feeArrangementForm.cc_number__c,
-    expiration_date: feeArrangementForm.cc_exp_date__c,
-    fee_agreement: feeArrangementForm.fee_schedule__c,
-    payment_method: feeArrangementForm.payment_method__c,
+  let upsertFeeArrangementParamters : Partial<salesforceSchema.fee_arrangement> =
+  {
+    cc_number: feeArrangementForm.cc_number,
+    cc_exp_date: feeArrangementForm.cc_exp_date,
+    fee_schedule: feeArrangementForm.fee_schedule,
+    payment_method: feeArrangementForm.payment_method,
     token: token
   }
 
@@ -116,42 +85,25 @@ function updateWelcomeForm(token: string, welcomeParameters: welcomePageParamete
 }
 
 function updateAppId(token : string, applicantForm : applicantIdForm): queryParameters{
-    let addresses: {'mailing': addressSchema, 'legal': addressSchema} = generateAddress(applicantForm);
-    let identification: identificationSchema = generateIdentification(applicantForm);
     let upsertApplicantv2 : Partial<salesforceSchema.applicant> = applicantForm
     console.log(upsertApplicantv2)
-    upsertApplicantv2.mailing_address = addresses.mailing;
-    upsertApplicantv2.legal_address = addresses.legal;
-    upsertApplicantv2.identification = identification;
     upsertApplicantv2.token = token;
     return generateQueryString('applicant', upsertApplicantv2, 'token')
 }
 
 function updateBeneficiaries(token: string, beneficiaryData: beneficiaryForm): queryParameters{
   let beneCount = beneficiaryData.beneficiary_count
-  let beneficiaryDataList : Array<salesforceSchema.beneficiary> = [];
+  let beneficiaryDataList : Array<Partial<salesforceSchema.beneficiary>> = [];
   let count = 0;
   beneficiaryData.beneficiaries.forEach(bene =>{
+    let pgBene : Partial<salesforceSchema.beneficiary> =  bene
     count++;
     console.log('dob')
-    console.log(bene.beneficiary_dob)
-    let beneficiaries : salesforceSchema.beneficiary ={
-      address: generateBeneAddress(bene),
-      beneficiary_type: bene.beneficiary_type,
-      date_of_birth: bene.beneficiary_dob,
-      email: bene.beneficiary_email,
-      first_name: bene.beneficiary_first_name,
-      last_name: bene.beneficiary_last_name,
-      middle_name: '',
-      phone: bene.beneficiary_phone,
-      relationship: bene.beneficiary_relationship,
-      share_percentage: bene.beneficiary_share === '' ? 0 : parseFloat(bene.beneficiary_share),
-      social_security_number: bene.beneficiary_ssn,
-      position: count,
-      bene_uuid: token + count.toString(),
-      token: token
-    }
-    beneficiaryDataList.push(beneficiaries);
+    console.log(bene.dob)
+    pgBene.position = count;
+    pgBene.bene_uuid = token+count;
+    pgBene.token = token;
+    beneficiaryDataList.push(pgBene);
   })
 
   return generateQueryStringFromList('beneficiary', beneficiaryDataList, 'bene_uuid');
@@ -215,49 +167,6 @@ function generateAddress(applicantForm: applicantIdForm): {'mailing': addressSch
     }
   }
   return resultAddress;
-}
-
-function generateBeneAddress(beneForm: beneficiary): addressSchema{
-  let resultAddress : addressSchema ={
-    address: beneForm.beneficiary_street,
-    city: beneForm.beneficiary_city,
-    state: beneForm.beneficiary_state,
-    zip: beneForm.beneficiary_zip
-
-  }
-  return resultAddress;
-}
-
-function generateIPAddress(accountNotificationsForm: accountNotificationsForm): addressSchema{
-  let resultAddress: addressSchema ={
-    address : accountNotificationsForm.interested_party_street__c,
-    city : accountNotificationsForm.interested_party_city__c,
-    state : accountNotificationsForm.interested_party_state__c,
-    zip : accountNotificationsForm.interested_party_zip__c
-  }
-  return resultAddress;
-}
-
-function generateTransferFormAddress(transfer: transfer){
-  let resultAddress: addressSchema ={
-    address: transfer.ira_street,
-    city: transfer.ira_city,
-    state: transfer.ira_state,
-    zip: transfer.ira_zip
-  }
-  return resultAddress;
-}
-
-function generateIdentification(applicantForm: applicantIdForm): identificationSchema{
-  let resultId: identificationSchema ={
-    expirationDate: applicantForm.expiration_date,
-    idNumber: applicantForm.id_number,
-    idType: applicantForm.id_type,
-    issueDate: applicantForm.issue_date,
-    issuedBy: applicantForm.issued_by
-  }
-
-  return resultId;
 }
 
 function runQuery(queryString: queryParameters, res: express.Response, client: pg.Client)
