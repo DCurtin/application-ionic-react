3cdb7225a01b17c3612394e63e85dc840fd3d46b
Dalton Curtin <daltoncurtin@gmail.com>
2020-06-17 16:05:46 -0400

    Added interfaces for initial investment type

--- client/src/helpers/Utils.tsx
+++ client/src/helpers/Utils.tsx
@@ -175,25 +175,31 @@ export interface rollover{
 
 export interface initialInvestmentForm{
     initial_investment_type : string,
-        initial_investment_name?: string, //##
-        investment_contact_person?: string, //##
-        investment_contact_person_phone?: null, //##
-        investment_amount?: string, //##
+    initial_investment_name?: string, //##
+    investment_contact_person?: string, //##
+    investment_contact_person_phone?: string, //##
+    investment_amount?: number, //##
 }
 
 export interface initialInvestmentConditionalParameters{
+    existing_ira_transfer: boolean,
+    existing_employer_plan_rollover: boolean, 
+    new_ira_contribution: boolean,
+    
+    //contribution
+    new_contribution_amount?: number
+
+    //transfer
     ira_full_or_partial_cash_transfer_1?: string, //#
     ira_full_or_partial_cash_transfer_2?: string, //#
-    transfertype1?: string, 
-    transfertype2?:string, 
-    existing_ira_transfer?: false,
-    existing_employer_plan_rollover?: false, 
-    new_ira_contribution?: false,
-    ira_cash_amount_1?: string,
-    ira_cash_amount_2?: string, 
-    employer_cash_amount_1?: string, 
-    employer_cash_amount_2?: string,
-    new_contribution_amount?: number
+    transfer_type_1?: string, 
+    transfer_type_2?:string, 
+    ira_cash_amount_1?: number,
+    ira_cash_amount_2?: number, 
+
+    //rollover
+    employer_cash_amount_1?: number, 
+    employer_cash_amount_2?: number,
 }
 
 export const states = [ 'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FL', 'GA',  'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY' ];
