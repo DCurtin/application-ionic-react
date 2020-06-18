02d15f3c710dbb4d2fa91e3e262caf12ae70f369
Dalton Curtin <daltoncurtin@gmail.com>
2020-06-18 17:27:46 -0400

    Temporarily added some interfaces in to deal with the request response

--- client/src/pages/Resume.tsx
+++ client/src/pages/Resume.tsx
@@ -1,13 +1,29 @@
 import React, {useState, useEffect} from 'react';
+import {requestBody} from '../helpers/Utils'
 import {IonPage, IonHeader, IonToolbar, IonThumbnail, IonImg, IonTitle, IonContent, IonInput, IonButton} from '@ionic/react'
 
+interface resume{
+    last_name:string,
+    last_4_ssn:string,
+    date_of_birth:string,
+    email:string
+}
+
+
+interface resumeRequest{
+    appExternalId: string
+    data: resume
+}
+
 const Resume: React.FC<{setSessionId: Function}> = ({setSessionId}) => {
-    const [authInput, setAuthInput] = useState({
+    const [authInput, setAuthInput] = useState<resume>({
         last_name:'',
         last_4_ssn:'',
-        date_of_birth:undefined,
+        date_of_birth:'',
         email:''
     })
+
+    const [loading, setLoading] = useState<boolean>(false);
 
     
     function updateForm(event:any){
@@ -33,9 +49,36 @@ const Resume: React.FC<{setSessionId: Function}> = ({setSessionId}) => {
                 <IonInput placeholder='Last 4 Digits of Your SSN' title='Last 4 Digits of Your SSN' type='number' name='last_4_ssn' value={authInput.last_4_ssn} onIonInput={updateForm}></IonInput>
                 <IonInput title='Your Date of Birth' type='date' name='date_of_birth' value={authInput.date_of_birth} onIonInput={updateForm}></IonInput>
                 <IonInput placeholder='youremail@domain.com' title='Your Email Address' type='email' name='email' value={authInput.email} onIonInput={updateForm}></IonInput>
-                <IonButton>Submit</IonButton>
+                <IonButton onClick={()=>{authenticate(authInput, setLoading, setSessionId)}} disabled={loading}>Submit</IonButton>
             </IonContent>
     </IonPage>)
+}
+
+function authenticate(authInput: resume, setLoading: Function, setSessionId: Function) {
+    setLoading(true);
+    let url = '/resume'
+    let body :resumeRequest ={
+        appExternalId: '',
+        data: authInput
     }
+    let options ={
+        method : 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(body)
+    }
+    fetch(url, options).then((response: Response)=>{
+        if(response.ok){
+            response.json().then((data:any)=>{
+                console.log(data.sessionId);
+                setSessionId(data.sessionId);
+            })
+            setLoading(false);
+        }else{
+            console.log(response.text)
+        }
+        setLoading(false);
+    })
+}
+
 
 export default Resume
