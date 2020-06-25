f296be0aa06c93a882f5e3488c9b3986fb083bc2
Dalton Curtin <daltoncurtin@gmail.com>
2020-06-25 17:25:25 -0400

    Moved resume and validation code to helper files

--- server.ts
+++ server.ts
@@ -5,16 +5,15 @@ import express from 'express';
 import {transformBeneClientToServer} from './server/utils/transformBeneficiaries'
 import {transformTransferClientToServer} from './server/utils/transformTransfers'
 import {transformRolloverClientToServer} from './server/utils/transformRollovers'
+import {resumeApplication} from './server/utils/retrieveFromSalesforce'
 import * as getPageInfoHandlers from './server/utils/getPageInfoHandlers'
 import * as saveStateHandlers from './server/utils/saveStateHandlers'
 import * as applicationInterfaces from './client/src/helpers/Utils'
 import * as salesforceSchema from './server/utils/salesforce'
+import * as validatedPages from './server/utils/validatePages'
 import {createAppSession} from './server/utils/appSessionHandler';
 import jsforce, {Connection as jsfConnection} from 'jsforce'
 import pg, { Client, Connection } from 'pg'
-import { create } from 'domain';
-import { Session } from 'inspector';
-import { queryParameters } from './server/utils/helperSchemas';
 const { v4: uuidv4 } = require('uuid');
 var session = require('express-session');
 var router = require('express').Router();
@@ -225,121 +224,8 @@ app.get("*", function (req : Express.Response, res : express.Response) {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
-//need to move to generic location
-interface resume{
-  last_name:string,
-  last_4_ssn:string,
-  date_of_birth:string,
-  email:string
-}
-interface resumeRequest{
-  appExternalId: string
-  data: resume
-}
-
 app.post('/resume', (req: express.Request, res: express.Response)=>{
-  let authParams : resumeRequest = req.body;
-  let options :jsforce.ExecuteOptions ={}
-  let herokuToken = authParams.appExternalId
-
-  interface salesforceResumeResponse{
-    attributes:{
-      type:string,
-      url:string
-    }
-    Id:string,
-    AccountNew__c:string,
-    Last_Name__c:string,
-    Email__c:string,
-    SSN__c:string,
-    DOB__c:string,
-    heroku_token__c:string
-  }
-  serverConn.sobject('Online_Application__C').findOne({HerokuToken__c:herokuToken}, ['Id','AccountNew__c','Last_Name__c','Email__c', 'SSN__c', 'DOB__c', 'HerokuToken__c']).execute(options,(err, record : any)=>{
-    let salesforceOnlineApp:salesforceResumeResponse= record
-
-    console.log(salesforceOnlineApp)
-    let lastFourSocial = salesforceOnlineApp.SSN__c?.match(/\d{4}/)
-    if(lastFourSocial === null || lastFourSocial === undefined){
-      console.group('failed, application likely does not have ssn')
-      res.status(500).send('failed to authenticate');  
-    }
-
-    let dateOfBirthsMatch = authParams.data.date_of_birth === salesforceOnlineApp.DOB__c;
-    let emailsMatch = authParams.data.email.toLowerCase() === salesforceOnlineApp.Email__c.toLowerCase()
-    let lastNamesMatch = authParams.data.last_name.toLowerCase() === salesforceOnlineApp.Last_Name__c.toLowerCase()
-    let lastFourSocialMatch = authParams.data.last_4_ssn === lastFourSocial[0]
-
-    if(dateOfBirthsMatch && emailsMatch && lastNamesMatch && lastFourSocialMatch)
-    {
-      serverConn.sobject("Online_Application__c").retrieve(record.Id).then((onlineAppresult:any)=>{
-      let sessionId : string = uuidv4();
-      let bodyInfo : Partial<salesforceSchema.body> ={
-        has_read_diclosure: onlineAppresult.Disclosures_Viewed__c,
-        account_type: onlineAppresult.Account_Type__c,
-        transfer_form: onlineAppresult.Existing_IRA_Transfer__c,
-        rollover_form: onlineAppresult.Existing_Employer_Plan_Rollover__c,
-        cash_contribution_form: onlineAppresult.New_IRA_Contribution__c,
-        investment_type: onlineAppresult.Initial_Investment_Type__c,
-        referred_by: onlineAppresult.Referred_By__c,
-        session_id: sessionId
-      }
-      let bodyParams = saveStateHandlers.generateQueryString('body',bodyInfo,'session_id')
-      let ownerInfo : Partial<salesforceSchema.applicant> ={
-        application_id: onlineAppresult.Id,
-        account_number: onlineAppresult.AccountNew__c,
-        salutation: onlineAppresult.Salutation__c,
-        first_name: onlineAppresult.First_Name__c,
-        last_name: onlineAppresult.Last_Name__c,
-        ssn: onlineAppresult.SSN__c,
-        dob: onlineAppresult.DOB__c,
-        marital_status: onlineAppresult.Marital_Status__c,
-        mothers_maiden_name: onlineAppresult.Mother_s_Maiden_Name__c,
-        occupation: onlineAppresult.Occupation__c,
-        is_self_employed: onlineAppresult.IsSelfEmployed__c,
-        has_hsa: onlineAppresult.HasHSA__c,
-        id_type: onlineAppresult.ID_Type__c,
-        id_number: onlineAppresult.ID_Number__c,
-        id_issued_by: onlineAppresult.Issued_By__c,
-        id_issued_date: onlineAppresult.Issue_Date__c,
-        id_expiration_date: onlineAppresult.Expiration_Date__c,
-        legal_street: onlineAppresult.Legal_Address__c,
-        legal_city: onlineAppresult.Legal_City__c,
-        legal_state: onlineAppresult.Legal_State__c,
-        legal_zip: onlineAppresult.Legal_Zip__c,
-        home_and_mailing_address_different:onlineAppresult.Home_and_Mailing_Address_Different__c,
-        mailing_street: onlineAppresult.Mailing_Address__c,
-        mailing_city: onlineAppresult.Mailing_City__c,
-        mailing_state: onlineAppresult.Mailing_State__c,
-        mailing_zip: onlineAppresult.Mailing_Zip__c,
-        primary_phone: onlineAppresult.Primary_Phone__c,
-        preferred_contact_method: onlineAppresult.Preferred_Contact_Method__c,
-        email: onlineAppresult.Email__c,
-        alternate_phone: onlineAppresult.Alternate_Phone__c,
-        alternate_phone_type: onlineAppresult.Alternate_Phone_Type__c,
-        heroku_token: onlineAppresult.HerokuToken__c,
-        session_id:sessionId
-      }
-      let applicantQueryParams = saveStateHandlers.generateQueryString('applicant', ownerInfo, 'session_id')
-
-      let validatedPages : salesforceSchema.validated_pages = {
-        ...JSON.parse(onlineAppresult.HerokuValidatedPages__c),
-        session_id:sessionId}
-      let validatedPagesQueryParams = saveStateHandlers.generateQueryString('validated_pages', validatedPages, 'session_id')
-      
-      saveStateHandlers.runQueryReturnPromise(applicantQueryParams, client).then((result:pg.QueryResult)=>{
-        saveStateHandlers.runQueryReturnPromise(bodyParams, client).then((bodyResult: pg.QueryResult)=>{
-          saveStateHandlers.runQueryReturnPromise(validatedPagesQueryParams, client).then((validatedPagesResult: pg.QueryResult)=>{
-            createAppSession(salesforceOnlineApp.AccountNew__c, salesforceOnlineApp.Id, sessionId, client, userInstances, res)
-          })
-        })
-      })
-    })
-    return
-    }
-    console.group('fail')
-    res.status(500).send('failed to authenticate');
-  })
+  resumeApplication(client, userInstances, serverConn, req.body, res)
 })
 
 app.post('/startApplication', function(req : express.Request, res : express.Response){
@@ -452,47 +338,19 @@ app.post('/validatePage', function(req: express.Request, res: express.Response){
     res.status(500).send('invalid arguments');
     return
   }
-
-  let pageValidation : Partial<salesforceSchema.validated_pages> = {
-    ...packet.data,
-    session_id:sessionId
-  }
-  let queryParam :queryParameters = saveStateHandlers.generateQueryString('validated_pages',pageValidation,'session_id');
-  saveStateHandlers.runQueryReturnPromise(queryParam, client).then((response:pg.QueryResult)=>{
-    res.send('ok');
-  }).catch(err=>{
-    console.log(err);
-    console.log('failed to update validated pages table');
-    res.status(500).send('Failed to update validated pages');
-  })
-
+  validatedPages.saveValidatedPages(sessionId, packet.data, client, res);
 })
 
 app.post('/getValidatedPages', function(req: express.Request, res: express.Response){
   let packet : applicationInterfaces.requestBody = req.body;
   let sessionId : string = packet.session.sessionId;
-
   if(sessionId === undefined || sessionId === '')
   {
     console.log('sessionId or page not set in request');
     res.status(500).send('invalid arguments');
     return
   }
-
-  let validationQuery ={
-    text: 'SELECT * FROM salesforce.validated_pages WHERE session_id = $1',
-    values: [sessionId]
-  }
-  client.query(validationQuery).then(function(result:pg.QueryResult<salesforceSchema.validated_pages>){
-    let validationFields = result.rows[0];
-    console.log(validationFields)
-    res.send({data: validationFields});
-  }).catch(err=>{
-    console.log(err);
-    console.log('failed to update validated pages table');
-    res.status(500).send('Failed to update validated pages');
-  })
-
+  validatedPages.getValidatedPages(sessionId, client, res);
 })
 
 app.post('/saveApplication', function(req : express.Request, res : express.Response){
