34afe794b0b933ca47ac553d5658d34356fd6e4d
Michelle Yuen <myuen@midlandira.com>
2020-06-29 14:50:46 -0500

    omitting any unused vars

--- client/src/components/OwnerInformation.tsx
+++ client/src/components/OwnerInformation.tsx
@@ -2,10 +2,10 @@ import React, {useState, useEffect} from 'react';
 import {useForm} from 'react-hook-form';
 import { IonContent, IonText, IonGrid, IonRow, IonCol, IonItemDivider, IonLabel, IonSelect, IonSelectOption, IonInput,IonCheckbox, IonRadioGroup, IonRadio,IonItem } from '@ionic/react';
 import { useHistory } from 'react-router-dom';
-import { SessionApp, states, requestBody, applicantIdForm, saveApplicationId} from '../helpers/Utils';
+import { SessionApp, states, applicantIdForm} from '../helpers/Utils';
 import {getAppPage, saveAppPage} from '../helpers/CalloutHelpers';
 
-const OwnerInformation: React.FC<SessionApp> = ({sessionId, setSessionId, updateMenuSections, formRef}) => {
+const OwnerInformation: React.FC<SessionApp> = ({sessionId, updateMenuSections, formRef}) => {
     const history = useHistory();
     const {register, handleSubmit, watch, errors} = useForm({
         mode: 'onBlur',
